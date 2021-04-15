ae1c76a6ed844b3e86e2609885ded47e1736857c
BinChengZhao <binchengZhao@outlook.com>
2021-04-15 21:10:26 +0800

    Design tables.

---
+++ delicate-scheduler/migrations/mysql/2021-04-15-123323_init/down.sql
@@ -0,0 +1,11 @@
+-- This file should undo anything in `up.sql`
+DROP TABLE task;
+DROP TABLE task_log;
+DROP TABLE executor_processor;
+DROP TABLE executor_group;
+DROP TABLE executor_processor_group;
+DROP TABLE user_auth;
+DROP TABLE user_base;
+DROP TABLE user_login_log;
+DROP TABLE user_register_log;
+DROP TABLE user_info_update;
