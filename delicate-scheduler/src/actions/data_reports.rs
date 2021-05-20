b63a5af413c9cc33f63979b6aeae053d38aac37e
BinChengZhao <binchengZhao@outlook.com>
2021-05-20 19:39:29 +0800

    Fix Aggregate problems.

--- delicate-scheduler/src/actions/data_reports.rs
+++ delicate-scheduler/src/actions/data_reports.rs
@@ -17,6 +17,13 @@ async fn show_one_day_tasks_state(pool: ShareData<db::ConnectionPool>) -> HttpRe
                     let now = NaiveDateTime::from_timestamp(get_timestamp() as i64, 0);
                     let past_day = now - ChronoDuration::days(1);
 
+                    // the number of tasks started in a given hour, the
+                    // the number of tasks that ended normally at a certain time
+                    // the number of tasks that ended abnormally at a certain time
+                    // the number of tasks that timed out at a certain time
+                    // Number of manually cancelled tasks at a certain time
+                    // Update to add a limit so that only tasks with Running status can be modified.
+
                     // That's is ok.
                     // task_log::table
                     //     .select(
@@ -24,15 +31,14 @@ async fn show_one_day_tasks_state(pool: ShareData<db::ConnectionPool>) -> HttpRe
                     //     )
                     //     .filter(task_log::created_time.between(past_day, now));
 
-                    // Can't package in a tuple.
-                    // let task_state: Vec<model::TaskState> = task_log::table
-                    //     .select((
-                    //         task_log::created_time,
-                    //         task_log::status,
-                    //         // dsl::count(task_log::id),
-                    //     ))
-                    //     .filter(task_log::created_time.between(past_day, now))
-                    //     .load(&conn)?;
+                    // Fixby: https://github.com/diesel-rs/diesel/issues/1781.
+                    let task_state = task_log::table
+                        .select(&(
+                            (task_log::created_time, task_log::status),
+                            diesel::dsl::sql::<diesel::sql_types::BigInt>("count(task_log.id)")
+                        ))
+                        .filter(task_log::created_time.between(past_day, now));
+                        // .group_by((task_log::created_time, task_log::status));
 
                     todo!();
                 })
