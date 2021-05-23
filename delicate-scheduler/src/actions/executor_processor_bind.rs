0002c9830591db619010e6df4e0b9cad532f0675
Bincheng <binchengZhao@outlook.com>
2021-05-23 20:12:52 +0800

    Append `executor_processor_bind` relate.

--- delicate-scheduler/src/actions/executor_processor_bind.rs
+++ delicate-scheduler/src/actions/executor_processor_bind.rs
@@ -1,1 +1,106 @@
+use super::prelude::*;
 
+pub(crate) fn config(cfg: &mut web::ServiceConfig) {
+    cfg.service(show_executor_processor_binds)
+        .service(create_executor_processor_bind)
+        .service(update_executor_processor_bind)
+        .service(delete_executor_processor_bind);
+}
+
+#[post("/api/executor_processor_bind/create")]
+async fn create_executor_processor_bind(
+    web::Json(executor_processor_bind): web::Json<model::NewExecutorProcessorBind>,
+    pool: ShareData<db::ConnectionPool>,
+) -> HttpResponse {
+    use db::schema::executor_processor_bind;
+
+    if let Ok(conn) = pool.get() {
+        return HttpResponse::Ok().json(Into::<UnifiedResponseMessages<usize>>::into(
+            web::block(move || {
+                diesel::insert_into(executor_processor_bind::table)
+                    .values(&executor_processor_bind)
+                    .execute(&conn)
+            })
+            .await,
+        ));
+    }
+
+    HttpResponse::Ok().json(UnifiedResponseMessages::<()>::error())
+}
+
+#[post("/api/executor_processor_bind/list")]
+async fn show_executor_processor_binds(
+    web::Json(query_params): web::Json<model::QueryParamsExecutorProcessorBind>,
+    pool: ShareData<db::ConnectionPool>,
+) -> HttpResponse {
+    if let Ok(conn) = pool.get() {
+        return HttpResponse::Ok().json(Into::<
+            UnifiedResponseMessages<model::PaginateExecutorProcessorBind>,
+        >::into(
+            web::block::<_, _, diesel::result::Error>(move || {
+                let query_builder = model::ExecutorProcessorBindQueryBuilder::query_all_columns();
+
+                let executor_processor_binds = query_params
+                    .clone()
+                    .query_filter(query_builder)
+                    .paginate(query_params.page)
+                    .load::<model::ExecutorProcessorBind>(&conn)?;
+
+                let per_page = query_params.per_page;
+                let count_builder = model::ExecutorProcessorBindQueryBuilder::query_count();
+                let count = query_params
+                    .query_filter(count_builder)
+                    .get_result::<i64>(&conn)?;
+
+                Ok(
+                    model::executor_processor_bind::PaginateExecutorProcessorBind::default()
+                        .set_tasks(executor_processor_binds)
+                        .set_per_page(per_page)
+                        .set_total_page(count),
+                )
+            })
+            .await,
+        ));
+    }
+
+    HttpResponse::Ok().json(UnifiedResponseMessages::<model::PaginateTask>::error())
+}
+
+#[post("/api/executor_processor_bind/update")]
+async fn update_executor_processor_bind(
+    web::Json(executor_processor_bind): web::Json<model::ExecutorProcessorBind>,
+    pool: ShareData<db::ConnectionPool>,
+) -> HttpResponse {
+    if let Ok(conn) = pool.get() {
+        return HttpResponse::Ok().json(Into::<UnifiedResponseMessages<usize>>::into(
+            web::block(move || {
+                diesel::update(&executor_processor_bind)
+                    .set(&executor_processor_bind)
+                    .execute(&conn)
+            })
+            .await,
+        ));
+    }
+
+    HttpResponse::Ok().json(UnifiedResponseMessages::<usize>::error())
+}
+#[post("/api/executor_processor_bind/delete")]
+async fn delete_executor_processor_bind(
+    web::Path(executor_processor_bind_id): web::Path<i64>,
+    pool: ShareData<db::ConnectionPool>,
+) -> HttpResponse {
+    use db::schema::executor_processor_bind::dsl::*;
+
+    // TODO: Manybe soft-delete.
+    if let Ok(conn) = pool.get() {
+        return HttpResponse::Ok().json(Into::<UnifiedResponseMessages<usize>>::into(
+            web::block(move || {
+                diesel::delete(executor_processor_bind.find(executor_processor_bind_id))
+                    .execute(&conn)
+            })
+            .await,
+        ));
+    }
+
+    HttpResponse::Ok().json(UnifiedResponseMessages::<usize>::error())
+}
--- delicate-scheduler/src/db/mysql/model/executor_processor_bind.rs
+++ delicate-scheduler/src/db/mysql/model/executor_processor_bind.rs
@@ -12,7 +12,7 @@ pub struct ExecutorProcessorBind {
     weight: i16,
     status: i16,
     created_time: NaiveDateTime,
-    deleted_time: NaiveDateTime,
+    deleted_time: Option<NaiveDateTime>,
 }
 
 #[derive(Insertable, AsChangeset, Debug, Serialize, Deserialize)]
@@ -25,4 +25,90 @@ pub struct NewExecutorProcessorBind {
     status: i16,
     created_time: NaiveDateTime,
     deleted_time: NaiveDateTime,
+}
+
+
+#[derive(Debug, Default, Clone, Serialize, Deserialize)]
+pub(crate) struct QueryParamsExecutorProcessorBind {
+    id: Option<i64>,
+    group_id: Option<i64>,
+    executor_id: Option<i64>,
+    name: Option<String>,
+    status: Option<i16>,
+    pub(crate) per_page: i64,
+    pub(crate) page: i64,
+}
+
+#[derive(Debug, Default, Clone, Serialize, Deserialize)]
+pub(crate) struct PaginateExecutorProcessorBind {
+    executor_processor_binds: Vec<ExecutorProcessorBind>,
+    per_page: i64,
+    total_page: i64,
+}
+
+impl PaginateExecutorProcessorBind {
+    pub(crate) fn set_tasks(mut self, executor_processor_binds: Vec<ExecutorProcessorBind>) -> Self {
+        self.executor_processor_binds = executor_processor_binds;
+        self
+    }
+
+    pub(crate) fn set_per_page(mut self, per_page: i64) -> Self {
+        self.per_page = per_page;
+        self
+    }
+
+    pub(crate) fn set_total_page(mut self, total: i64) -> Self {
+        self.total_page = (total as f64 / self.per_page as f64).ceil() as i64;
+        self
+    }
+}
+
+pub(crate) struct ExecutorProcessorBindQueryBuilder;
+impl ExecutorProcessorBindQueryBuilder {
+    pub(crate) fn query_all_columns() -> executor_processor_bind::BoxedQuery<'static, Mysql> {
+        executor_processor_bind::table
+            .into_boxed()
+            .select(executor_processor_bind::all_columns)
+    }
+
+    pub(crate) fn query_count(
+    ) -> executor_processor_bind::BoxedQuery<'static, Mysql, diesel::sql_types::Bigint> {
+        executor_processor_bind::table.into_boxed().count()
+    }
+}
+
+impl QueryParamsExecutorProcessorBind {
+    pub(crate) fn query_filter<ST>(
+        self,
+        mut statement_builder: executor_processor_bind::BoxedQuery<'static, Mysql, ST>,
+    ) -> executor_processor_bind::BoxedQuery<'static, Mysql, ST> {
+        statement_builder = statement_builder.filter(executor_processor_bind::status.ne(2));
+        // Maybe status 2 eq task-deleted status.
+
+        if let Some(executor_processor_bind_id) = self.id {
+            statement_builder =
+                statement_builder.filter(executor_processor_bind::id.eq(executor_processor_bind_id));
+        }
+
+        if let Some(executor_processor_bind_group_id) = self.group_id {
+            statement_builder =
+                statement_builder.filter(executor_processor_bind::group_id.eq(executor_processor_bind_group_id));
+        }
+
+        if let Some(executor_processor_bind_executor_id) = self.executor_id {
+            statement_builder =
+                statement_builder.filter(executor_processor_bind::executor_id.eq(executor_processor_bind_executor_id));
+        }
+        
+        if let Some(executor_processor_bind_name) = self.name {
+            statement_builder =
+                statement_builder.filter(executor_processor_bind::name.like(executor_processor_bind_name));
+        }
+        if let Some(task_status) = self.status {
+            statement_builder =
+                statement_builder.filter(executor_processor_bind::status.eq(task_status));
+        }
+
+        statement_builder.order(executor_processor_bind::id.desc())
+    }
 }
