5427dc8ce2f69a948fabfcfd12797f4c5e96a0e5
BinChengZhao <binchengZhao@outlook.com>
2021-07-29 17:45:13 +0800

    Optimize sql.

--- delicate-scheduler/migrations/mysql/2021-07-27-110601_rich_logs/up.sql
+++ delicate-scheduler/migrations/mysql/2021-07-27-110601_rich_logs/up.sql
@@ -5,7 +5,7 @@ CREATE TABLE operation_log (
 `operation_type` tinyint(4) NOT NULL DEFAULT '1' COMMENT 'Operation type: 1 add 2 modify 3 delete',
 `user_id` bigint(20) NOT NULL DEFAULT '0' COMMENT 'Operation user id',
 `user_name` varchar(64) NOT NULL DEFAULT '' COMMENT 'Operation user name',
-`operation_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'Operation time'
+`operation_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'Operation time',
 PRIMARY KEY (`id`),
 KEY `idx_table_id` (`table_id`) USING BTREE,
 KEY `idx_operation_time` (`operation_time`) USING BTREE,
@@ -14,10 +14,10 @@ KEY `idx_user_id_type_time` (`user_id`,`operation_type`,`operation_time`) USING 
 
 
 CREATE TABLE operation_log_detail (
-`id` bigint(20) NOT NULL PRIMARY KEY AUTO_INCREMENT COMMENT 'Self-incrementing id',
+`id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT 'Self-incrementing id',
 `operation_log_id` bigint(20) NOT NULL DEFAULT '0' COMMENT 'Operation log id',
 `column_comment` varchar(128) NOT NULL DEFAULT '' COMMENT 'Field Description',
-`values` varchar(128) NOT NULL DEFAULT '' COMMENT 'Values'
+`values` varchar(128) NOT NULL DEFAULT '' COMMENT 'Values',
 PRIMARY KEY (`id`),
 KEY `idx_operation_log_id` (`operation_log_id`) USING BTREE
 )ENGINE INNODB DEFAULT CHARSET=utf8mb4 COMMENT 'Operation log details table';
--- delicate-scheduler/src/macros/mod.rs
+++ delicate-scheduler/src/macros/mod.rs
@@ -1,3 +1,5 @@
+pub(crate) use super::prelude;
+
 #[macro_use]
 pub(crate) mod feature_cfg;
 pub(crate) mod helper;
